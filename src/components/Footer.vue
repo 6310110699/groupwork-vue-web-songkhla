dfe05f01939ec2d0dbfaca9d3beec80d636e3e68
6310110699 <90898175+6310110699@users.noreply.github.com>
2023-08-14 16:04:19 +0700

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-footer dark padless class="mt-5">
+    <v-card
+      flat
+      tile
+      class="primary white--text text-center align-self-md-stretch"
+      width="100%"
+    >
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-card-text class="white--text pt-0">
+        รายวิชา 240-420
+        | Assignment (Group work) <v-icon class="red--text">fa-heart</v-icon> 
+        <br />
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: () => ({
+    icons: [
+      "fab fa-facebook",
+      "fab fa-twitter",
+      "fab fa-google-plus",
+      "fab fa-linkedin",
+      "fab fa-instagram"
+    ]
+  })
+};
+</script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-parallax dark :src="heroData.hero.heroImgSrc">
+    <v-row align="center" justify="center">
+      <v-col style="color: #155555;">
+        <h1 class="display-2 font-weight-bold mb-4 text-center">{{heroData.hero.heroTitle}}</h1>
+        <br />
+        <h4 class="subheading text-center">{{heroData.hero.heroSubtitle}}</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+import heroData from "~/data/hero.yml";
+export default {
+  data() {
+    return {
+      heroData
+    };
+  }
+};
+</script>
